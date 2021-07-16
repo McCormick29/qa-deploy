cc12c8089e770eebe4de4af3686257b220eec995
Levi McCormick <levimccormick34@gmail.com>
2021-07-16 13:23:35 -0600

    origin

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -21,21 +21,27 @@ test('I can start a game', async () => {
 
 test('Add X to upper left corner', async () => {
 
-    let UpperLSqr = await (await driver).findElement(By.id('cell-0'));
-    await UpperLSqr.click();
+    let upperLSqr = await (await driver).findElement(By.id('cell-0'));
+    await upperLSqr.click();
+
+    expect(await upperLSqr.getText()).toBe('X')
     
 });
 
 test('Add X to upper right corner', async () => {
 
-    let UpperLSqr = await (await driver).findElement(By.id('cell-2'));
-    await UpperLSqr.click();
+    let upperRSqr = await (await driver).findElement(By.id('cell-2'));
+    await upperRSqr.click();
+
+    expect(await upperRSqr.getText()).toBe('X')
     
 });
 
 test('Add X to bottom right corner', async () => {
 
-    let UpperLSqr = await (await driver).findElement(By.id('cell-8'));
-    await UpperLSqr.click();
-    
+    let bottomRSqr = await (await driver).findElement(By.id('cell-8'));
+    await bottomRSqr.click();
+
+    expect(await bottomRSqr.getText()).toBe('X')
 });
+
